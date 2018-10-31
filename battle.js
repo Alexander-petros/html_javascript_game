9229cca8a4a13455b4e0cf9e61be5b8f429e672b
005524905 <005524905@csusb.edu@jb359-7.cse.csusb.edu>
2018-10-31 11:59:04 -0700

    added button functionality

--- battle.js
+++ battle.js
@@ -1,2 +1,26 @@
-//alert("Battle.js loaded");
-//Adding in the new battle ui and logic here
+var cLog = document.getElementById('combatLog');
+var atk = document.getElementById('atk');
+var def = document.getElementById('def');
+var skl = document.getElementById('skl');
+
+atk.onclick = function(){
+    console.log("Attack");
+    addCombatLog("Attacked");
+};
+
+def.onclick = function(){
+    console.log("Defend");
+    addCombatLog("Defended");
+};
+
+skl.onclick = function(){
+    console.log("Skill");
+    addCombatLog("Spec");
+};
+
+function addCombatLog(text){
+    let textnode = document.createTextNode(text);
+    cLog.appendChild(textnode);
+    let br = document.createElement("br");
+    cLog.appendChild(br);
+}
