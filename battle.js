a693d3b2d3e29d487aefda4d86f705d5cac2fc26
root <root@Alex-Surface.localdomain>
2018-11-06 16:55:36 -0800

    Updated battle variables

--- battle.js
+++ battle.js
@@ -2,6 +2,12 @@ var cLog = document.getElementById('combatLog');
 var atk = document.getElementById('atk');
 var def = document.getElementById('def');
 var skl = document.getElementById('skl');
+var uskl = document.getElementById('uskl');
+var player = {
+    attack:     10,
+    defense:    10,
+    magic:      10
+};
 
 atk.onclick = function(){
     console.log("Attack");
@@ -16,6 +22,11 @@ def.onclick = function(){
 skl.onclick = function(){
     console.log("Skill");
     addCombatLog("Spec");
+};
+
+uskil.onclick = function(){
+    console.log("Ultimate skill");
+    addCombatLog("Ultimate Skill");
 };
 
 function addCombatLog(text){
