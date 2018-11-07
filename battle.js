9730865443a9955eed45f0d6ff9262c4c03e1581
root <root@Alex-Surface.localdomain>
2018-11-07 09:54:11 -0800

    Added hp effect

--- battle.js
+++ battle.js
@@ -3,15 +3,19 @@ var atk = document.getElementById('atk');
 var def = document.getElementById('def');
 var skl = document.getElementById('skl');
 var uskl = document.getElementById('uskl');
+var pHp = document.getElementById('playerHpText');
 var player = {
-    attack:     10,
+    strength:   10,
     defense:    10,
-    magic:      10
+    magic:      10,
+    hp:         20,
+    mhp:        20
 };
 
 atk.onclick = function(){
     console.log("Attack");
     addCombatLog("Attacked");
+    modifyPlayerHp(10);
 };
 
 def.onclick = function(){
@@ -24,10 +28,22 @@ skl.onclick = function(){
     addCombatLog("Spec");
 };
 
-uskil.onclick = function(){
+uskl.onclick = function(){
     console.log("Ultimate skill");
     addCombatLog("Ultimate Skill");
 };
+
+function modifyPlayerHp(damage){
+    if (damage >= player.hp){
+        pHp.innerHTML = "Dead";
+        player.hp = 0;
+    }
+    else{
+        console.log(player.hp);
+        player.hp = player.hp - damage;
+        pHp.innerHTML = player.hp;
+    }
+}
 
 function addCombatLog(text){
     let textnode = document.createTextNode(text);
